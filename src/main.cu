49e828b01080470054eeda4d1237aca97571fcf9
hehelego <2364261262@qq.com>
2021-12-22 23:53:00 +0800

    upd(main): test curand performance

--- src/main.cu
+++ src/main.cu
@@ -13,5 +13,17 @@ double time_once(std::function<void()> func) {
   timer.end();
   return timer.time_in_second();
 }
+double time_func(std::function<void()> func, u32 runs) {
+  double s = 0;
+  for (u32 i = 0; i < runs; i++) s += time_once(func);
+  return s / runs;
+}
 
-i32 main() { return 0; }
+const u32 N = 1 << 25;
+i32 main() {
+  auto rand_keys = mallocDevice<u32>(N);
+  auto f = [&]() { batchRandomGen(rand_keys, N); };
+  auto span = time_func(f, 20u);
+  Output() << "takes" << span << "seconds for one generation\n";
+  return 0;
+}
