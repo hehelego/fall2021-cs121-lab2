23c9290f26175a016dd3ae77f06cef4a60eb1b58
hehelego <2364261262@qq.com>
2021-12-25 01:43:37 +0800

    upd(scripts)

--- all.fish
+++ all.fish
@@ -1,7 +1,7 @@
 #!/usr/bin/fish
 
 set GPU_SM_ARCH 37 # tested on Kepler K80
-set BUILD_TYPE Release
+set BUILD_TYPE Debug
 
 cmake -D CMAKE_CUDA_ARCHITECTURES=$GPU_SM_ARCH -D CMAKE_BUILD_TYPE=$BUILD_TYPE .
 make -j8
---
+++ copy-src.fish
@@ -0,0 +1,2 @@
+#!/usr/bin/fish
+rsync -azvhP --delete src/ ../../coding/hpc-home/lab2/src/
